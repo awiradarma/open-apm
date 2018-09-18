2d755a0584bb8c83a7874339432d52946f407b6a
Andre Wiradarma <awiradarma@gmail.com>
2018-09-18 13:57:32 -0500

    Add author service to run on wildfly v12

---
+++ author/author-svc/src/main/java/test/andre/author/Activator.java
@@ -0,0 +1,9 @@
+package test.andre.author;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class Activator extends Application {
+
+}
